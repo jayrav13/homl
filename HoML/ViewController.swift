d21dd806c24f1e92dcf85031cc8e1a148f50b6cd
Jay Ravaliya <jayrav13@gmail.com>
2016-01-24 01:02:28 -0500

    Huge updates on ViewController

--- HoML/ViewController.swift
+++ HoML/ViewController.swift
@@ -27,7 +27,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     var currentUser : JSON!
     var userMatches : JSON!
     
+    var helloLabel : UILabel!
+    
     var dangerButton : UIButton!
+    var dangerBox : UILabel!
+    
+    var activityIndicator : UIActivityIndicatorView!
     
     override func viewDidLoad() {
         
@@ -37,8 +42,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
         
         // Basics
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.whiteColor()
-        self.title = "HOME"
+        self.view.backgroundColor = UIColor(red: 59/255, green: 75/255, blue: 56/255, alpha: 1)
+        self.title = "storyteller"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName : UIFont(name: "Mentone-Semibold", size: 20)!]
         self.userMatches = []
         
         // Location Setup
@@ -47,6 +53,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
         self.locationManager.requestAlwaysAuthorization()
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
+        
+        self.helloLabel = UILabel()
+        self.helloLabel.frame = CGRect(x: 0, y: Standard.screenHeight * 0.15, width: Standard.screenWidth, height: Standard.screenHeight * 0.10)
+        self.helloLabel.textAlignment = NSTextAlignment.Center
+        self.helloLabel.font = UIFont(name: "OpenSans-Semibold", size: 30)
+        self.helloLabel.textColor = UIColor(red: 151/255, green: 189/255, blue: 142/255, alpha: 1)
+        // self.view.addSubview(self.helloLabel)
         
         // Add Buttons
         self.historyBarButtonItem = UIBarButtonItem(title: "History", style: UIBarButtonItemStyle.Plain, target: self, action: "pushToHistoryViewController:")
@@ -57,22 +70,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
         
         // If Location Services enabled, set up timer.
         if(CLLocationManager.locationServicesEnabled()) {
-            locationTimer = NSTimer.scheduledTimerWithTimeInterval(30 , target: self, selector: "sendLocation", userInfo: nil, repeats: true)
+            locationTimer = NSTimer.scheduledTimerWithTimeInterval(30, target: self, selector: "sendLocation", userInfo: nil, repeats: true)
         }
         
         // UI Elements - do not add yet
         self.saidHelloToLabel = UILabel()
         self.saidHelloToLabel.frame = CGRect(x: 0, y: Standard.screenHeight * 0.25, width: Standard.screenWidth, height: Standard.screenHeight * 0.05)
-        self.saidHelloToLabel.text = "You could have said \"Hello\" to..."
+        self.saidHelloToLabel.textColor = UIColor(red: 151/255, green: 189/255, blue: 142/255, alpha: 1)
+        self.saidHelloToLabel.font = UIFont(name: "OpenSans-Semibold", size: 18)
+        self.saidHelloToLabel.text = "today, you could have said hello to..."
         self.saidHelloToLabel.adjustsFontSizeToFitWidth = true
         self.saidHelloToLabel.textAlignment = NSTextAlignment.Center
         
         self.biosTableView = UITableView()
-        self.biosTableView.frame = CGRect(x: Standard.screenWidth * 0.05, y: Standard.screenHeight * 0.40, width: Standard.screenWidth * 0.90, height: Standard.screenHeight * 0.15 * 3)
+        self.biosTableView.frame = CGRect(x: 0, y: Standard.screenHeight * 0.40, width: Standard.screenWidth, height: Standard.screenHeight * 0.10 * 3)
         self.biosTableView.dataSource = self
         self.biosTableView.delegate = self
         self.biosTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.biosTableView.scrollEnabled = false
+        self.biosTableView.separatorStyle = UITableViewCellSeparatorStyle.None
         
         self.goToProfileLabel = UILabel()
         self.goToProfileLabel.frame = CGRect(x: Standard.screenWidth * 0.1, y: Standard.screenHeight * 0.40, width: Standard.screenWidth * 0.8, height: Standard.screenHeight * 0.20)
@@ -80,11 +96,24 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
         self.goToProfileLabel.adjustsFontSizeToFitWidth = true
         self.goToProfileLabel.textAlignment = NSTextAlignment.Center
         
+        self.dangerBox = UILabel()
+        self.dangerBox.frame = CGRect(x: 0, y: Standard.screenHeight * 0.9, width: Standard.screenWidth, height: Standard.screenHeight * 0.1)
+        self.dangerBox.backgroundColor = UIColor.whiteColor()
+        // self.view.addSubview(self.dangerBox)
+        
         self.dangerButton = UIButton(type: UIButtonType.System)
-        self.dangerButton.frame = CGRect(x: Standard.screenWidth * 0.30, y: Standard.screenHeight * 0.9, width: Standard.screenWidth * 0.4, height: Standard.screenHeight * 0.05)
-        self.dangerButton.setTitle("DangerView", forState: UIControlState.Normal)
+        self.dangerButton.frame = CGRect(x: 0, y: Standard.screenHeight * 0.9, width: Standard.screenWidth, height: Standard.screenHeight * 0.1)
+        self.dangerButton.setTitle("Safety Status", forState: UIControlState.Normal)
         self.dangerButton.addTarget(self, action: "pushToDangerViewController:", forControlEvents: UIControlEvents.TouchUpInside)
-        self.view.addSubview(self.dangerButton)
+        self.dangerButton.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 0.5)
+        self.dangerButton.setTitleColor(UIColor(red: 59/255, green: 75/255, blue: 56/255, alpha: 1), forState: UIControlState.Normal)
+        self.dangerButton.titleLabel?.font = UIFont(name: "OpenSans-Semibold", size: 18)
+        // self.view.addSubview(self.dangerButton)
+        
+        self.activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
+        self.activityIndicator.frame = CGRect(x: Standard.screenWidth / 2 - Standard.screenWidth * 0.05, y: Standard.screenHeight / 2 - Standard.screenWidth * 0.05, width: Standard.screenWidth * 0.1, height: Standard.screenHeight * 0.1)
+        self.activityIndicator.alpha = 0
+        self.view.addSubview(self.activityIndicator)
         
         self.loadUIElements()
     }
@@ -96,7 +125,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -109,7 +137,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     func sendLocation() {
         API.sendLocation((self.locationManager.location?.coordinate.latitude)!, longitude: (self.locationManager.location?.coordinate.longitude)!) { (success, data) -> Void in
             if(success) {
-                print("Sent location!")
+                
+                if(data["sum"].doubleValue <= 10.0) {
+                    self.dangerButton.backgroundColor = UIColor(red: 28/255, green: 136/255, blue: 72/255, alpha: 0.5)
+                }
+                else if(data["sum"].doubleValue > 10.0 && data["sum"].doubleValue <= 20.0) {
+                    self.dangerButton.backgroundColor = UIColor(red: 255/255, green: 171/255, blue: 0, alpha: 0.5)
+                }
+                else {
+                    self.dangerButton.backgroundColor = UIColor(red: 255/255, green: 7/255, blue: 0, alpha: 0.5)
+                }
+                
             }
             else {
                 print("Error sending location!")
@@ -126,7 +164,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     }
     
     func pushToProfileViewController(sender : UIButton) {
-        // NSAPI.setProfileAddedSetting(true)
         let pvc : ProfileViewController = ProfileViewController()
         if(self.currentUser != nil) {
             pvc.userData = self.currentUser
@@ -137,13 +174,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell : UITableViewCell = self.biosTableView.dequeueReusableCellWithIdentifier("cell")!
         cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.textColor = UIColor(red: 151/255, green: 189/255, blue: 142/255, alpha: 1)
         if(userMatches.count != 0) {
             cell.textLabel?.text = self.userMatches["matches"][indexPath.row]["match"]["bio"].stringValue
-            cell.detailTextLabel?.text = self.userMatches["matches"][indexPath.row]["match"]["story"].stringValue
+            // cell.detailTextLabel?.text = self.userMatches["matches"][indexPath.row]["match"]["story"].stringValue
         }
         else {
             cell.textLabel?.text = "Test"
         }
+        cell.backgroundColor = UIColor(red: 59/255, green: 75/255, blue: 56/255, alpha: 1)
         cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
         return cell
     }
@@ -166,23 +205,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     }
 
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return Standard.screenHeight * 0.15
+        return Standard.screenHeight * 0.10
     }
     
     func loadUIElements() {
+        self.activityIndicator.alpha = 1
+        self.activityIndicator.startAnimating()
         if(NSAPI.getProfileAddedSetting()) {
-            self.view.addSubview(self.saidHelloToLabel)
-            self.view.addSubview(self.biosTableView)
             API.getUser({ (success, data) -> Void in
                 if(success) {
                     for(var i = 0; i < data["users"].count; i++) {
                         if(data["users"][i]["number"].stringValue == API.number) {
                             self.currentUser = data["users"][i]
+                            self.helloLabel.text = "Hello, " + self.currentUser["username"].stringValue
+                            self.activityIndicator.alpha = 0
+                            self.activityIndicator.stopAnimating()
                             break
                         }
                     }
-                    //  print(self.currentUser)
-                    // self.currentUser = data
+                    
+                    self.biosTableView.reloadData()
+                    self.view.addSubview(self.helloLabel)
+                    self.view.addSubview(self.biosTableView)
+                    self.view.addSubview(self.saidHelloToLabel)
+                    self.view.addSubview(self.dangerBox)
+                    self.view.addSubview(self.dangerButton)
                 }
                 else {
                     print("Error - user data")
